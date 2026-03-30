2f4413939c910e6c91ae9564e92cbf5cea2c1f87
Pedro Antonio Nava Hernández <panh98@hotmail.com>
2026-03-30 15:59:51 -0600

    Add files via upload

---
+++ Proyectos/OPBRIDGE CENEVAL/index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+
+
+    <link rel="stylesheet" href="css/estilos.css">
+    <link rel="stylesheet" href="css/texto.css">
+  
+    <title>OP Bridge Ceneval ECD</title>
+
+
+</head>
+<body>
+        <img src="img/Especialistas en contacto directo.png" class="img-ECD">
+    <img src="img/Ceneval.png" class="img-Ceneval">
+
+   
+
+
+    <h1 id="h1">BIENVENIDO</h1>
+    <h2>Aqui puedes realizar tus consultas en tu campaña</h2>
+
+
+
+
+
+     <div class="contenedor-imgs">
+    <a href="games.html">
+        <img src="img/juegos.png" class="img-games">
+    </a>
+
+    <a href="asistencia.html">
+        <img src="img/asistencia.jpg" class="img-asistance">
+    </a>
+
+    <a href="llamadas.html">
+        <img src="img/llamadas ejemplo.jpg" class="img-callexample">
+    </a>
+
+     <a href="Speachceneval.html">
+        <img src="img/speach para atc.png" class="img-atc">
+    </a>
+
+      <a href="https://ceneval.edu.mx/examendesdecasa/">
+        <img src="img/Navegador Seguro.jpg" class="img-NS">
+    </a>
+
+
+</div>
+
+
+
+
+</div>
+</body>
+</html>
