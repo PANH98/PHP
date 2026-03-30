38c866e399a29e5dd91ef98f7176b73d37af2712
Pedro Antonio Nava Hernández <panh98@hotmail.com>
2026-03-30 13:14:50 -0600

    Add files via upload

---
+++ Proyectos/CRUD/autoload.php
@@ -0,0 +1,25 @@
+<?php
+#SE MUESTRA LA RUTA RAÍZ DEL DOCUMENTO 
+  spl_autoload_register(function($clase){
+
+$archivo=__DIR__."/".$clase.".php";
+$archivo= str_replace("\\","/",$archivo);
+
+if(is_file($archivo)){
+require_once $archivo;
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+  });
